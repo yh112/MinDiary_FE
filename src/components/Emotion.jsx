62ff7ee3dbb03104bdc240be09ca01d8a301dcd6
yh112 <msh970011@naver.com>
2024-07-24 15:28:46 +0900

    Sytle: 랜딩 이미지 변경 및 5번 페이지 css 수정

--- src/components/Emotion.jsx
+++ src/components/Emotion.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
-function Emotion({emotionName, emotionSrc, percent}) {
-  return <div>
-    <img src={emotionSrc}/>
-    <p>{emotionName}{emotionSrc}</p>
-    <p>{percent}</p>
-  </div>;
+function Emotion({ emotionName, emotionSrc, percent }) {
+  return (
+    <div className="emotion-content">
+      <img src={emotionSrc} />
+      <div className="emotion-title">
+        <p>{emotionName}</p>
+        <p style={{fontWeight:"bold"}}>{percent}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Emotion;
